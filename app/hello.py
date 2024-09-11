30d247ea026e391ef2e4f7ccdf26ddb4078b0b39
nyamanoi <n.yamanoi@offgrid.co.jp>
2024-09-11 16:16:15 +0900

    Flask-SQLAlchemyからpsycopg2に書き換え

--- app/hello.py
+++ app/hello.py
@@ -1,25 +1,23 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from psycopg2.extensions import connection
+import random, string
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ny88NY99@localhost/testdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# DB接続
+def get_connection() -> connection:
+    return psycopg2.connect('postgresql://postgres:ny88NY99@localhost:5432/testdb')
 
-db = SQLAlchemy(app)
+# 関数
+def randomname(n):
+   return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
 
-# モデルの定義
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-# テーブルの作成
-with app.app_context():
-    db.create_all()
+def fetch_data_as_dict(cursor):
+    """カスタムキー名で辞書形式に変換する関数"""
+    columns = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
 
 # ルートの定義
 # メニュー
@@ -32,20 +30,20 @@ def index():
 def login_get():
     return render_template('login.html')
 
-@app.post('/login')
-def login_post():
-    username = request.form["username"]
-    email = request.form["email"]
+# @app.post('/login')
+# def login_post():
+#     username = request.form["username"]
+#     email = request.form["email"]
 
-    loginUser = User.query.filter(User.username == username).filter(User.email == email).first()
+#     loginUser = User.query.filter(User.username == username).filter(User.email == email).first()
 
-    if not loginUser:
-        message = "ログイン情報が間違っています。"
-        return render_template('login.html', message=message, username=username, email=email)
-    else:
-        message = "ログインしました。"
-        users = User.query.all()
-        return render_template('index.html', message=message, users=users)
+#     if not loginUser:
+#         message = "ログイン情報が間違っています。"
+#         return render_template('login.html', message=message, username=username, email=email)
+#     else:
+#         message = "ログインしました。"
+#         users = User.query.all()
+#         return render_template('index.html', message=message, users=users)
 
 # パスワードリセット
 @app.get('/password_reset')
@@ -60,7 +58,11 @@ def password_reset_post():
 # ユーザーマスタ一覧
 @app.get("/master")
 def master_get():
-    users = User.query.all()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT user_id, name, mail, created_at FROM users')
+            users = fetch_data_as_dict(cur)
+
     return render_template('master.html', users=users)
 
 # 新規登録
@@ -70,16 +72,23 @@ def new_get():
 
 @app.post('/new')
 def new_post():
+    userId = randomname(6)
     username = request.form["username"]
     email = request.form["email"]
+    password = request.form["password"]
 
-    newUser = User(username=username, email=email)
-    db.session.add(newUser)
-    db.session.commit()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            query = 'INSERT INTO users (user_id, name, mail, password, status) VALUES (%s, %s, %s, %s, %s)'
+            cur.execute(query, (userId, username, email, password, '有'))
+        conn.commit()
 
     message = "ユーザー登録が完了しました。"
-    users = User.query.all()
-    return render_template('index.html', message=message, users=users)
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM users')
+            users = cur.fetchall()
+    return render_template('master.html', message=message, users=users)
 
 # 更新、削除
 @app.get('/update/<id>')
