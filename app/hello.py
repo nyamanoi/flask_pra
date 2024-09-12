16a7bdc734b67b1a3aa9d4b04dddb34faaf3d7e5
nyamanoi <n.yamanoi@offgrid.co.jp>
2024-09-12 10:01:24 +0900

    新規登録修正

--- app/hello.py
+++ app/hello.py
@@ -68,14 +68,29 @@ def new_get():
 @app.post('/new')
 def new_post():
     userId = randomname(6)
+
     username = request.form["username"]
     email = request.form["email"]
     password = request.form["password"]
+
+    gender = request.form["gender"]
+    postCode = request.form["post_code"]
+    address = request.form["address"]
+    phoneNumber = request.form["phone_number"]
+
+    shikakuCode = randomname(2)
+    license = request.form["license"]
 
     with get_connection() as conn:
         with conn.cursor() as cur:
             query = 'INSERT INTO users (user_id, name, mail, password, status) VALUES (%s, %s, %s, %s, %s)'
             cur.execute(query, (userId, username, email, password, '有'))
+            query = 'INSERT INTO user_info (user_id, gender, yubin, jyusyo, tel, status) VALUES (%s, %s, %s, %s, %s, %s)'
+            cur.execute(query, (userId, gender, postCode, address, phoneNumber, '有'))
+            query = 'INSERT INTO shikaku (shikaku_code, shikaku_name) VALUES (%s, %s)'
+            cur.execute(query, (shikakuCode, license))
+            query = 'INSERT INTO user_shikaku (user_id, shikaku_code) VALUES (%s, %s)'
+            cur.execute(query, (userId, shikakuCode))
         conn.commit()
 
     message = "ユーザー登録が完了しました。"
@@ -83,6 +98,7 @@ def new_post():
         with conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute('SELECT user_id, name, mail, created_at FROM users')
             users = cur.fetchall()
+
     return render_template('master.html', message=message, users=users)
 
 # 更新、削除
@@ -100,8 +116,25 @@ def update_get(id):
 @app.post('/update/<id>')
 def update_post(id):
     if request.form['method'] == 'PUT':
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        gender = request.form["gender"]
+        postCode = request.form["post_code"]
+        address = request.form["address"]
+        phoneNumber = request.form["phone_number"]
+        license = request.form["license"]
+
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                query = 'UPDATE users SET (user_id, name, mail, password, status) VALUES (%s, %s, %s, %s, %s)'
+                cur.execute(query, (userId, username, email, password, '有'))
+            conn.commit()
+
         return render_template('update.html', id='更新です')
+
     elif request.form['method'] == 'DELETE':
         return render_template('update.html', id='削除です')
+
     else:
         return render_template('update.html', id='例外エラーです')
