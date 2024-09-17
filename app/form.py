25b639c1b60932e1bf02ff46a6a6cf14f188cb91
nyamanoi <n.yamanoi@offgrid.co.jp>
2024-09-17 15:27:34 +0900

    登録にバリデーションチェック追加

---
+++ app/form.py
@@ -0,0 +1,68 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, ValidationError, RadioField, PasswordField
+from wtforms.validators import DataRequired, Email, Optional
+
+
+class Form(FlaskForm):
+    email = StringField(
+        "メールアドレス",
+        validators=[
+            DataRequired(),
+            Email(message="メールアドレスの形式で入力してください。"),
+        ],
+        render_kw={"placeholder": "xxx@xxx.com"},
+    )
+    name = StringField(
+        "名前",
+        validators=[
+            DataRequired(),
+        ],
+    )
+    password = PasswordField(
+        "パスワード",
+        validators=[
+            DataRequired(),
+        ],
+    )
+    gender = RadioField(
+        "性別", choices=[("男", "男"), ("女", "女")], validators=[Optional()]
+    )
+    post_code = StringField(
+        "郵便番号", validators=[Optional()], render_kw={"placeholder": "000-0000"}
+    )
+    address = StringField(
+        "住所", validators=[Optional()], render_kw={"placeholder": "〇〇県〇〇市"}
+    )
+    phone_number = StringField(
+        "電話番号",
+        validators=[Optional()], render_kw={"placeholder": "000-000-0000"}
+    )
+    license = StringField("取得資格", validators=[Optional()])
+
+    def validate_email(self, email):
+        if len(email.data) > 30:
+            raise ValidationError("30文字以内で入力してください。")
+
+    def validate_name(self, name):
+        if len(name.data) > 16:
+            raise ValidationError("16文字以内で入力してください。")
+
+    def validate_password(self, password):
+        if len(password.data) < 8:
+            raise ValidationError("8文字以上で入力してください。")
+
+    def validate_post_code(self, post_code):
+        if len(post_code.data) > 8:
+            raise ValidationError("8文字以内で入力してください。")
+
+    def validate_address(self, address):
+        if len(address.data) > 50:
+            raise ValidationError("50文字以内で入力してください。")
+
+    def validate_phone_number(self, phone_number):
+        if len(phone_number.data) > 12:
+            raise ValidationError("12文字以内で入力してください。")
+
+    def validate_license(self, license):
+        if len(license.data) > 30:
+            raise ValidationError("30文字以内で入力してください。")
--- app/hello.py
+++ app/hello.py
@@ -4,6 +4,7 @@ from psycopg2.extensions import connection
 from psycopg2.extras import DictCursor
 import random
 import string
+from form import Form
 
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
@@ -205,56 +206,67 @@ def master_get():
 # 新規登録画面表示
 @app.get("/new")
 def new_get():
-    return render_template("new.html")
+    form = Form()
+    return render_template("new.html", form=form)
 
 
 # 新規登録処理
 @app.post("/new")
 def new_post():
-    # 新規登録処理
-    userId = randomname(6)
-    username = request.form["username"]
-    email = request.form["email"]
-    password = request.form["password"]
-    gender = request.form["gender"]
-    postCode = request.form["post_code"]
-    address = request.form["address"]
-    phoneNumber = request.form["phone_number"]
-    shikakuCode = randomname(2)
-    license = request.form["license"]
+    form = Form()
+    # バリデーションチェック
+    if form.validate_on_submit():
+        # 新規登録処理
+        userId = randomname(6)
+        username = form.name.data
+        email = form.email.data
+        password = form.password.data
+        gender = form.gender.data
+        postCode = form.post_code.data
+        address = form.address.data
+        phoneNumber = form.phone_number.data
+        shikakuCode = randomname(2)
+        license = form.license.data
 
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            query = """
-                INSERT
-                INTO users(user_id, name, mail, password, status)
-                VALUES (%s, %s, %s, %s, %s)
-            """
-            cur.execute(query, (userId, username, email, password, "有"))
-            query = """
-                INSERT
-                INTO user_info(user_id, gender, yubin, jyusyo, tel, status)
-                VALUES (%s, %s, %s, %s, %s, %s)
-            """
-            cur.execute(query, (userId, gender, postCode,
-                        address, phoneNumber, "有"))
-            query = """
-                INSERT
-                INTO shikaku(shikaku_code, shikaku_name)
-                VALUES (%s, %s)
-            """
-            cur.execute(query, (shikakuCode, license))
-            query = """
-                INSERT
-                INTO user_shikaku(user_id, shikaku_code)
-                VALUES (%s, %s)
-            """
-            cur.execute(query, (userId, shikakuCode))
-        conn.commit()
+        print("いいいいいいいいいいいいいい", gender)
 
-    message = "ユーザー登録が完了しました。"
-    users = userGetAll()
-    return render_template("master.html", message=message, users=users)
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                query = """
+                    INSERT
+                    INTO users(user_id, name, mail, password, status)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                cur.execute(query, (userId, username, email, password, "有"))
+                query = """
+                    INSERT
+                    INTO user_info(user_id, gender, yubin, jyusyo, tel, status)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                """
+                cur.execute(query, (userId, gender, postCode,
+                            address, phoneNumber, "有"))
+                query = """
+                    INSERT
+                    INTO shikaku(shikaku_code, shikaku_name)
+                    VALUES (%s, %s)
+                """
+                cur.execute(query, (shikakuCode, license))
+                query = """
+                    INSERT
+                    INTO user_shikaku(user_id, shikaku_code)
+                    VALUES (%s, %s)
+                """
+                cur.execute(query, (userId, shikakuCode))
+            conn.commit()
+
+        message = "ユーザー登録が完了しました。"
+        users = userGetAll()
+        return render_template("master.html", message=message, users=users)
+    else:
+        errorMessage = '入力エラーがあります。'
+        return render_template(
+            'new.html', form=form, errorMessage=errorMessage
+        )
 
 
 # 更新、削除画面表示
